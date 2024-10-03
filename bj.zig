e1635b880d6cba164469528f43b7344cf09bb5da
Greg Donald <gd@darkclear.net>
2024-10-03 16:05:35 -0500

    Deal new hand

--- bj.zig
+++ bj.zig
@@ -35,7 +35,8 @@ const CountMethod = enum {
 };
 
 pub const HandStatus = enum(u8) {
-    Won = 1,
+    Unknown = 0,
+    Won,
     Lost,
     Push,
 };
@@ -61,12 +62,12 @@ pub const Game = struct {
     current_player_hand: u8,
     total_player_hands: u8,
     quitting: bool,
-    shuffle_specs: *const [2]u8,
+    shuffle_specs: *const [8][2]u8,
     faces: *const [4]*const u8,
     faces2: *const [4]*const u8,
 };
 
-const shuffle_specs: [8][2]u8 = [8][2]u8{
+pub const shuffle_specs: [8][2]u8 = [8][2]u8{
     [2]u8{ 95, 8 },
     [2]u8{ 92, 7 },
     [2]u8{ 89, 6 },
@@ -165,4 +166,107 @@ pub fn new_shoe(game: *Game, values: []const u8, values_count: u32) !void {
     }
 
     try shuffle(&game.shoe);
+}
+
+pub fn need_to_shuffle(game: *const Game) bool {
+    if (game.shoe.num_cards == 0) {
+        return true;
+    }
+
+    const current_card: f64 = @as(f64, @floatFromInt(game.shoe.current_card));
+    const num_cards: f64 = @as(f64, @floatFromInt(game.shoe.num_cards));
+    const ratio: f64 = current_card / num_cards * 100.0;
+    const used: u32 = @as(u32, @intFromFloat(ratio));
+
+    for (0..MAX_DECKS) |x| {
+        if (game.num_decks == game.shuffle_specs[x][1] and used > game.shuffle_specs[x][0]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+pub fn new_regular(game: *Game) !void {
+    const values = [13]u8{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    try new_shoe(game, &values, 13);
+}
+
+pub fn build_new_shoe(game: *Game) !void {
+    // switch (game.deck_type) {
+    //     2 => {
+    //         new_aces(game);
+    //     },
+    //     3 => {
+    //         new_jacks(game);
+    //     },
+    //     4 => {
+    //         new_aces_jacks(game);
+    //     },
+    //     5 => {
+    //         new_sevens(game);
+    //     },
+    //     6 => {
+    //         new_eights(game);
+    //     },
+    //     else => {
+    try new_regular(game);
+    //     },
+    // }
+}
+
+pub fn deal_card(shoe: *Shoe, hand: *Hand) void {
+    hand.cards[hand.num_cards] = shoe.cards[shoe.current_card];
+    hand.num_cards += 1;
+    shoe.current_card += 1;
+}
+
+pub fn deal_new_hand(game: *Game) !void {
+    var player_hand = PlayerHand{
+        .hand = Hand{
+            .cards = [_]Card{Card{ .value = 0, .suit = 0 }} ** MAX_CARDS_PER_HAND,
+            .num_cards = 0,
+        },
+        .bet = game.current_bet,
+        .stood = false,
+        .played = false,
+        .paid = false,
+        .status = HandStatus.Unknown,
+    };
+    var dealer_hand = &game.dealer_hand;
+    const shoe = &game.shoe;
+
+    if (need_to_shuffle(game)) {
+        try build_new_shoe(game);
+    }
+
+    dealer_hand.hide_down_card = true;
+    dealer_hand.hand.num_cards = 0;
+
+    deal_card(shoe, &player_hand.hand);
+    deal_card(shoe, &dealer_hand.hand);
+    deal_card(shoe, &player_hand.hand);
+    deal_card(shoe, &dealer_hand.hand);
+
+    game.player_hands[0] = player_hand;
+    game.current_player_hand = 0;
+    game.total_player_hands = 1;
+
+    // if (dealer_upcard_is_ace(dealer_hand) && !is_blackjack(&player_hand.hand)) {
+    //     draw_hands(game);
+    //     ask_insurance(game);
+    //     return;
+    // }
+
+    // if (player_is_done(game, &player_hand)) {
+    //     dealer_hand.hide_down_card = false;
+    //     pay_hands(game);
+    //     draw_hands(game);
+    //     bet_options(game);
+    //     return;
+    // }
+
+    // draw_hands(game);
+    // player_get_action(game);
+    // save_game(game);
 }
